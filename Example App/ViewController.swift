3517eed353b7576263c5c1f9ea1d1b7c3de6068f
Chinmay Deshpande <cadeshpa@indiana.edu>
2015-07-20 17:45:18 -0400

    function to calculate monthly expenses
    Signed-off-by: Chinmay Deshpande <cadeshpa@indiana.edu>

--- Example App/ViewController.swift
+++ Example App/ViewController.swift
@@ -10,8 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var outputLabel: UILabel!
+    
+    @IBOutlet var discountInput: UITextField!
+    @IBOutlet var textInput: UITextField!
+    @IBOutlet var button: UIButton!
+    
+    @IBOutlet var resultLabel: UILabel!
+    @IBAction func button(sender: AnyObject) {
+        
+       var enteredValue = textInput.text.toInt()
+        
+        var discountValue = discountInput.text.toInt()
+        
+        if enteredValue != nil && discountValue != nil {
+            
+        
+        var temp = (enteredValue! * discountValue!)/100
+        
+        var yearlyspend = enteredValue!-temp
+        
+        var monthlySpend = yearlyspend/12
+        
+        resultLabel.text = "You should spend $ \(monthlySpend) monthly"
+        }
+        
+            
+        else {
+        
+            resultLabel.text = "Please enter both annual package and % saving"
+            
+        }
+        
+        
+        
+        
+    
+        
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       // button.frame=CGRectMake(80, 380,160, 30)
+        
+        println("hey")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
